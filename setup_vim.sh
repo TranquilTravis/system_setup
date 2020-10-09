afc8e8747b5b50d14fe9216a90da6ccf62e0b127
dongqi <dongqi.dotabi@bytedance.com>
2020-10-09 18:16:51 +0800

    mods

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,4 +1,8 @@
-cd ~/.vim/bundle/YouCompleteMe
+# install vim8.2 either by brew install or make install from git repo
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo 'prompt_context() {}' &>> ~/.oh-my-zsh/custom/themes/powerlevel9k/powerlevel9k.zsh-theme
+cd ~/.vim/bundle/youcompleteme
 python3 install.py --all
-apt-get install silversearcher-ag
-apt-get install ripgrep
+brew install silversearcher-ag
+brew install ripgrep
